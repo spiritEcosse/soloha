9fbd7beafd31a7caa81ca3013ee796d59b3ab7c3
taras <taraslegend@mail.com>
2016-06-22 17:05:23 +0300

    add site map

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -133,6 +133,7 @@ class InfoPage(FlatPage):
     meta_title = models.CharField(verbose_name=_('Meta tag: title'), blank=True, max_length=255)
     meta_description = models.TextField(verbose_name=_('Meta tag: description'), blank=True)
     meta_keywords = models.TextField(verbose_name=_('Meta tag: keywords'), blank=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         app_label = 'flatpages'
---
+++ sitemap.py
@@ -0,0 +1,38 @@
+from django.contrib.sitemaps import Sitemap
+from apps.catalogue.models import Product
+from apps.catalogue.models import Category
+from apps.catalogue.models import InfoPage
+
+
+class ProductSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.5
+
+    def items(self):
+        return Product.objects.filter(enable=True)
+
+    def lastmod(self, obj):
+        return obj.date_updated
+
+
+class CategorySitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.5
+
+    def items(self):
+        return Category.objects.filter(enable=True)
+
+    def lastmod(self, obj):
+        return obj.created
+
+
+class InfoPageSitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 0.5
+
+    def items(self):
+        return InfoPage.objects.filter(enable=True)
+
+    def lastmod(self, obj):
+        return obj.created
+
