57bfaa4028af39077d79fb66322f9f6550056b4f
igor <shevchenkcoigor@gmail.com>
2016-07-13 16:50:29 +0300

    add migrate filer

--- apps/catalogue/management/commands/migrate_filer.py
+++ apps/catalogue/management/commands/migrate_filer.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
 
 ##
 ## FancyImageModel is a plugin model holding a ImageField called "oldimage" and
@@ -24,11 +25,22 @@ class Command(BaseCommand):
                 print fancy.original_image, "needs migration"
                 name = os.path.basename(fancy.original_image.name)
 
-                if not os.path.exists('{}/{}'.format(MEDIA_ROOT, name)):
+                if not os.path.exists(name):
                     name = None
 
                 if name is not None:
                     i = name_image_map.get(name)
+                    # image = slugify(name.split('/'))
+                    # path = image[:-1]
+                    # image = image[-1].split('.')
+
+                    # if len(image) == 2:
+                    #     extend = image[-1]
+                    #     name_file = image[0]
+                    #     new_name_file = slugify(name_file)
+                    #     new_name_file = '{}.{}'.format(new_name_file, extend)
+                    #     path.append(new_name_file)
+                    #     new_file = '/'.join(path)
 
                     if i is None:
                         i = Image(
