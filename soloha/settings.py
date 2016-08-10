eb8bf87a40af3ea0d3055e7155ab30bb84a51222
igor <shevchenkcoigor@gmail.com>
2016-08-10 11:41:36 +0300

    change primary_color

--- soloha/settings.py
+++ soloha/settings.py
@@ -93,11 +93,11 @@ TEMPLATES = [
         ],
         'OPTIONS': {
             'loaders': [
-                ('django.template.loaders.cached.Loader', [
+                # ('django.template.loaders.cached.Loader', [
                     'django.template.loaders.filesystem.Loader',
                     'django.template.loaders.app_directories.Loader',
                     'django.template.loaders.eggs.Loader',
-                ]),
+                # ]),
             ],
             'context_processors': [
                 'django.template.context_processors.debug',
