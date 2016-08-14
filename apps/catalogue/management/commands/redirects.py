f4df44dd46f2cf27671a59eac2bbd67802eeb5ce
igor <shevchenkcoigor@gmail.com>
2016-08-14 18:57:44 +0300

    uncomment categories

--- apps/catalogue/management/commands/redirects.py
+++ apps/catalogue/management/commands/redirects.py
@@ -15,38 +15,36 @@ class Command(BaseCommand):
         :return:
         """
         current_site = Site.objects.get(pk=1)
-        # Redirect.objects.all().delete()
 
-        # for category in Category.objects.all():
-        #     old_path = u'/{}/'.format(category.full_slug)
-        #     new_path = category.get_absolute_url()
-        #     print old_path
-        #     print new_path
-        #     Redirect.objects.create(site=current_site, old_path=old_path, new_path=new_path)
-        #
-        # print '\n end categories \n '
-        #
+        for category in Category.objects.all():
+            old_path = u'/{}/'.format(category.full_slug)
+            new_path = category.get_absolute_url()
+            print old_path
+            print new_path
+            Redirect.objects.create(site=current_site, old_path=old_path, new_path=new_path)
+
+        print '\n end categories \n '
+
         count_products = Product.objects.count()
 
         for key, product in enumerate(Product.objects.all()):
             left_products = count_products - key
 
-            if left_products < 1997:
-                print 'left products - {}'.format(left_products)
+            print 'left products - {}'.format(left_products)
 
-                if product.categories.first() is not None:
-                    old_path = u'/{}/{}'.format(product.categories.first().full_slug, product.slug)
+            if product.categories.first() is not None:
+                old_path = u'/{}/{}'.format(product.categories.first().full_slug, product.slug)
 
-                    try:
-                        new_path = product.get_absolute_url()
-                    except NoReverseMatch:
-                        product.slug = slugify(product.slug)
-                        product.save()
-                        new_path = product.get_absolute_url()
+                try:
+                    new_path = product.get_absolute_url()
+                except NoReverseMatch:
+                    product.slug = slugify(product.slug)
+                    product.save()
+                    new_path = product.get_absolute_url()
 
-                    print old_path
-                    print new_path
-                    Redirect.objects.create(site=current_site, old_path=old_path, new_path=new_path)
+                print old_path
+                print new_path
+                Redirect.objects.create(site=current_site, old_path=old_path, new_path=new_path)
 
         Redirect.objects.all().update(new_path=F(slugify('new_path')))
         Category.objects.all().update(slug=F(slugify('slug')))
