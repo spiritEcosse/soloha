c6db7f878e9a25f0e13036750b8a32e99a24b73c
igor <shevchenkcoigor@gmail.com>
2016-09-01 17:51:41 +0300

    not print page

--- apps/catalogue/management/commands/redirects_filters.py
+++ apps/catalogue/management/commands/redirects_filters.py
@@ -31,10 +31,8 @@ class Command(BaseCommand):
         """
         url = 'http://soloha.kiev.ua'
         main_page = urllib2.urlopen(url)
-        print main_page
         soup_main_page = BeautifulSoup(main_page.read())
-        time.sleep(2)
-        categories = soup_main_page.find(id='topnav2')
+        categories = soup_main_page.select('#topnav2')
 
         for category_a in categories.find_all('a')[1:]:
             category_link = category_a.attrs['href']
