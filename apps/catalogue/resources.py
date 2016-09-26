857c41eec74170a0cd7a15fbea139be992726586
igor <shevchenkcoigor@gmail.com>
2016-09-26 15:45:49 +0300

    fix

--- apps/catalogue/resources.py
+++ apps/catalogue/resources.py
@@ -122,8 +122,168 @@ class ModelResource(resources.ModelResource):
     )
     prefix = 'rel_'
 
+    def __new__(cls, *args, **kwargs):
+        obj = super(ModelResource, cls).__new__(cls, *args, **kwargs)
+
+        for field in obj.get_fields():
+            if isinstance(field.widget, widgets.IntermediateModelManyToManyWidget):
+                field.widget.rel_field = obj._meta.model._meta.get_field_by_name(field.attribute)[0]
+
+                field.widget.intermediate_own_fields = []
+
+                for rel_field in field.widget.rel_field.rel.through._meta.fields:
+                    if rel_field is not field.widget.rel_field.rel.through._meta.pk and not isinstance(rel_field, ForeignKey):
+                        field.widget.intermediate_own_fields.append(rel_field)
+
+        return obj
+
+    @classmethod
+    def widget_from_django_field(cls, f, default=import_export_widgets.Widget):
+        """
+        Returns the widget that would likely be associated with each
+        Django type.
+        """
+        result = default
+        internal_type = f.get_internal_type()
+        if internal_type in ('ManyToManyField', ):
+            result = functools.partial(widgets.IntermediateModelManyToManyWidget,
+                                       model=f.rel.to, rel=f.rel)
+        if internal_type in ('ForeignKey', 'OneToOneField', ):
+            result = functools.partial(import_export_widgets.ForeignKeyWidget,
+                                       model=f.rel.to)
+        if internal_type in ('DecimalField', ):
+            result = import_export_widgets.DecimalWidget
+        if internal_type in ('DateTimeField', ):
+            result = import_export_widgets.DateTimeWidget
+        elif internal_type in ('DateField', ):
+            result = import_export_widgets.DateWidget
+        elif internal_type in ('IntegerField', 'PositiveIntegerField',
+                               'PositiveSmallIntegerField',
+                               'SmallIntegerField', 'AutoField'):
+            result = import_export_widgets.IntegerWidget
+        elif internal_type in ('BooleanField', 'NullBooleanField'):
+            result = import_export_widgets.BooleanWidget
+        return result
+
+    @classmethod
+    def field_from_django_field(self, field_name, django_field, readonly):
+        """
+        Returns a Resource Field instance for the given Django model field.
+        """
+
+        FieldWidget = self.widget_from_django_field(django_field)
+        widget_kwargs = self.widget_kwargs_for_field(field_name)
+        field = Field(attribute=field_name, column_name=field_name, widget=FieldWidget(**widget_kwargs),
+                      readonly=readonly)
+        return field
+
     def for_delete(self, row, instance):
         return self.fields['delete'].clean(row)
+
+    def copy_relation(self, obj):
+        for field in self.get_fields():
+            if isinstance(field.widget, widgets.IntermediateModelManyToManyWidget) \
+                    or isinstance(field.widget, import_export_widgets.ManyToManyWidget) \
+                    or isinstance(field.widget, widgets.ImageManyToManyWidget):
+                setattr(obj, '{}{}'.format(self.prefix, field.column_name), self.export_field(field, obj))
+
+    def import_row(self, row, instance_loader, dry_run=False, **kwargs):
+        """
+        Imports data from ``tablib.Dataset``. Refer to :doc:`import_workflow`
+        for a more complete description of the whole import process.
+
+        :param row: A ``dict`` of the row to import
+
+        :param instance_loader: The instance loader to be used to load the row
+
+        :param dry_run: If ``dry_run`` is set, or error occurs, transaction
+            will be rolled back.
+        """
+        try:
+            row_result = self.get_row_result_class()()
+            instance, new = self.get_or_init_instance(instance_loader, row)
+            if new:
+                row_result.import_type = RowResult.IMPORT_TYPE_NEW
+            else:
+                row_result.import_type = RowResult.IMPORT_TYPE_UPDATE
+            row_result.new_record = new
+            row_result.object_repr = force_text(instance)
+            row_result.object_id = instance.pk
+            original = deepcopy(instance)
+            self.copy_relation(original)
+
+            if self.for_delete(row, instance):
+                if new:
+                    row_result.import_type = RowResult.IMPORT_TYPE_SKIP
+                    row_result.diff = self.get_diff(None, None, dry_run)
+                else:
+                    row_result.import_type = RowResult.IMPORT_TYPE_DELETE
+                    self.delete_instance(instance, dry_run)
+                    row_result.diff = self.get_diff(original, None, dry_run)
+            else:
+                self.import_obj(instance, row, dry_run)
+                if self.skip_row(instance, original):
+                    row_result.import_type = RowResult.IMPORT_TYPE_SKIP
+                else:
+                    with transaction.atomic():
+                        self.save_instance(instance, dry_run)
+                    self.save_m2m(instance, row, dry_run)
+                    # Add object info to RowResult for LogEntry
+                    row_result.object_repr = force_text(instance)
+                    row_result.object_id = instance.pk
+                row_result.diff = self.get_diff(original, instance, dry_run)
+        except Exception as e:
+            # There is no point logging a transaction error for each row
+            # when only the original error is likely to be relevant
+            if not isinstance(e, TransactionManagementError):
+                logging.exception(e)
+            tb_info = traceback.format_exc()
+            row_result.errors.append(self.get_error_result_class()(e, tb_info, row))
+        return row_result
+
+    def get_diff(self, original, current, dry_run=False):
+        """
+        Get diff between original and current object when ``import_data``
+        is run.
+
+        ``dry_run`` allows handling special cases when object is not saved
+        to database (ie. m2m relationships).
+        """
+        data = []
+        dmp = diff_match_patch()
+        for field in self.get_fields():
+            attr = '{}{}'.format(self.prefix, field.column_name)
+
+            if hasattr(original, attr):
+                v1 = getattr(original, attr)
+            else:
+                v1 = self.export_field(field, original) if original else ""
+
+            v2 = self.export_field(field, current) if current else ""
+
+            diff = dmp.diff_main(force_text(v1), force_text(v2))
+            dmp.diff_cleanupSemantic(diff)
+            html = dmp.diff_prettyHtml(diff)
+            html = mark_safe(html)
+            data.append(html)
+        return data
+
+    def save_m2m(self, obj, data, dry_run):
+        """
+        Saves m2m fields.
+
+        Model instance need to have a primary key value before
+        a many-to-many relationship can be used.
+        """
+        if not dry_run:
+            for field in self.get_fields():
+                field.widget.obj = obj
+
+                if not isinstance(field.widget, import_export_widgets.ManyToManyWidget) \
+                        and not isinstance(field.widget, widgets.ImageManyToManyWidget) \
+                        and not isinstance(field.widget, widgets.IntermediateModelManyToManyWidget):
+                    continue
+                self.import_field(field, obj, data)
 
 
 class FeatureResource(ModelResource):
--- apps/partner/resources.py
+++ apps/partner/resources.py
@@ -11,7 +11,7 @@ StockRecord = get_model('partner', 'StockRecord')
 
 class StockRecordResource(resources.ModelResource):
     product = fields.Field(
-        attribute='product', column_name='Product title',
+        attribute='product', column_name='product',
         widget=widgets.ForeignKeyWidget(model=Product, field='slug')
     )
     attributes = fields.Field(
