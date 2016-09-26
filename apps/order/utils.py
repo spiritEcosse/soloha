b8886a19e9e31591535e3495ff7d05c2fa93510d
igor <shevchenkcoigor@gmail.com>
2016-09-26 19:04:28 +0300

    checkout/preview part 1

---
+++ apps/order/utils.py
@@ -0,0 +1,22 @@
+from oscar.core.loading import get_model
+from oscar.core.loading import get_class
+from oscar.apps.order.utils import OrderCreator as CoreOrderCreator
+
+
+Order = get_model('order', 'Order')
+Line = get_model('order', 'Line')
+OrderDiscount = get_model('order', 'OrderDiscount')
+order_placed = get_class('order.signals', 'order_placed')
+
+
+class OrderCreator(CoreOrderCreator):
+    def create_line_attributes(self, order, order_line, basket_line):
+        """
+        Creates the batch line attributes.
+        """
+        for attr in basket_line.attributes.all():
+            order_line.attributes.create(
+                option=attr.option,
+                type=attr.option.code,
+                value=attr.value
+            )
