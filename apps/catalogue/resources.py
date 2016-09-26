d91390766126454f0e599fdab5432c0dbc15cd81
igor <shevchenkcoigor@gmail.com>
2016-09-26 16:38:58 +0300

    fix

--- apps/catalogue/resources.py
+++ apps/catalogue/resources.py
@@ -295,7 +295,16 @@ class ModelResource(resources.ModelResource):
             if isinstance(field.widget, widgets.ManyToManyWidget):
                 continue
             self.import_field(field, obj, data)
-            print getattr(obj, field)
+            print getattr(obj, str(field))
+
+    def import_field(self, field, obj, data):
+        """
+        Calls :meth:`import_export.fields.Field.save` if ``Field.attribute``
+        and ``Field.column_name`` are found in ``data``.
+        """
+        if field.attribute and field.column_name in data:
+            print field.attribute and field.column_name in data
+            field.save(obj, data)
 
 
 class FeatureResource(ModelResource):
