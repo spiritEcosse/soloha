b8024279dba50a19ed060b995c22a48c2692d282
igor <shevchenkcoigor@gmail.com>
2016-12-01 15:02:01 +0200

    delete oscar, transfer code
    Former-commit-id: 380ff7183ce8f448784d4e85ff03da0ffda7250d

--- apps/basket/app.py
+++ apps/basket/app.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from soloha.core.application import Application
 
+from apps.basket.views import BasketAddWithAttributesView
 from apps.basket.views import BasketView, SavedView, BasketAddView, VoucherAddView, VoucherRemoveView
 
 
@@ -13,6 +14,7 @@ class BasketApplication(Application):
     add_view = BasketAddView
     add_voucher_view = VoucherAddView
     remove_voucher_view = VoucherRemoveView
+    add_view_with_attributes = BasketAddWithAttributesView
 
     def get_urls(self):
         urls = [
@@ -21,6 +23,10 @@ class BasketApplication(Application):
             url(r'^vouchers/add/$', self.add_voucher_view.as_view(), name='vouchers-add'),
             url(r'^vouchers/(?P<pk>\d+)/remove/$', self.remove_voucher_view.as_view(), name='vouchers-remove'),
             url(r'^saved/$', login_required(self.saved_view.as_view()), name='saved'),
+            url(
+                r'^add_with_attributes/(?P<pk>\d+)/$', self.add_view_with_attributes.as_view(),
+                name='add_with_attributes'
+            ),
         ]
         return self.post_process_urls(urls)
 
