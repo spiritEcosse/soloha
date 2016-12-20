15f9941ad9eb7d65ebc8a8c574072985fcc80e94
igor <shevchenkcoigor@gmail.com>
2016-12-20 18:22:27 +0200

    autocommit datetime on 2016-12-20 18:22:26
    Former-commit-id: c890d2ac261a39663a2867c0d03e8aba7027cc7b

---
+++ dump.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function dumpdata() {
+    echo "Here, the recent actions of the this script."
+    source /home/h5782c/virtualenv/zapchastie/2.7/bin/activate
+    ./manage.py dumpdata --indent 4 --natural-primary --natural-foreign -e contenttypes -e auth.Permission -e sessions -e admin > data/fixtures/all.json
+    git add .
+    git commit -m "autocommit datetime on `date +'%Y-%m-%d %H:%M:%S'` (dump database and images)"
+    git push
+}
+
+dumpdata 2>&1 | tee error.log
