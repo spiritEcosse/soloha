91d1433297678ba3f941b8dceb23a55fdcd01ab7
igor <shevchenkcoigor@gmail.com>
2017-01-24 17:44:10 +0200

    add icons to phones
    Former-commit-id: a6f2efa3f94af76428227bf377b1b2a3addee7cb

--- apps/ex_flatpages/context_processors.py
+++ apps/ex_flatpages/context_processors.py
@@ -10,7 +10,6 @@ def context_data(request):
         Prefetch('info__phone_numbers', queryset=PhoneNumber.objects.browse()),
     ).get()
     context['current_site'] = site
-    print site.info.phone_numbers.all()
 
     flatpages = site.flatpage_set.select_related('info').filter(info__position__in=('header', 'footer')).only(
         'url',
