afb0345c84ec5fe21060de8ac5526d60d41f4380
igor <shevchenkcoigor@gmail.com>
2017-03-21 18:14:35 +0200

    repo /home/igor/web/soloha; br python3; date 2017-03-21 18:14:35
    Former-commit-id: 1af95ed5c8614e2f20f40aa18faf8115c700a4c7

--- entrypoint-base.sh
+++ entrypoint-base.sh
@@ -10,4 +10,5 @@ GOSU_USER=spirit
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m $GOSU_USER
 export HOME=/home/$GOSU_USER
 
+echo 'start with' + USER_ID
 exec /usr/local/bin/gosu $GOSU_USER "$@"
