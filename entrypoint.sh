368688fc38925724bb7f7d5b3034ff2943b9857f
igor <shevchenkcoigor@gmail.com>
2017-03-24 16:55:01 +0200

    repo /home/igor/web/soloha; br python3; date 2017-03-24 16:55:01
    Former-commit-id: b65cc3eaba3a4a170437634628f3379ed005a8c0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
@@ -10,5 +10,5 @@ GOSU_USER=spirit
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m $GOSU_USER
 export HOME=/home/$GOSU_USER
 
-echo 'start with' + USER_ID
+echo "start with : $USER_ID : $GOSU_USER"
 exec /usr/local/bin/gosu $GOSU_USER "$@"
